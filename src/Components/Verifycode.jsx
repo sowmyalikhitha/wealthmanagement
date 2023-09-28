655620a0f9ee1ecd619a3ae2d0eb271c7b7f2cf2
Imandi <sowmya-likhitha.imandi@capgemini.com>
2023-09-28 11:22:12 +0530

    created verify code component

--- src/Components/Verifycode.jsx
+++ src/Components/Verifycode.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import wifi from './Images/wifi.jpg';
+import image from './Images/bgimage.png';
 
 function Verifycode (){
    
     return (
+        <div className="body">
+        <div className="container1">
+            <p>22:00 | 100MB/s</p>
+            <img className="wifi" src={wifi} alt="wifi"></img>
+        </div>
         <div>
-            <h1>VERIFICATION CODE</h1>   
+        <img className="bgimage" src={image} alt="image"></img> 
+        </div>
+        <h1>Forgot password</h1>
+        <form className="formelement">
+            <label>Enter code</label><br />
+            <input type="text" maxLength="1" size="1" min="0" max="9" pattern="[0-9]{1}"></input>
+            <input type="text" maxLength="1" size="1" min="0" max="9" pattern="[0-9]{1}"></input>
+            <input type="text" maxLength="1" size="1" min="0" max="9" pattern="[0-9]{1}"></input>
+            <input type="text" maxLength="1" size="1" min="0" max="9" pattern="[0-9]{1}"></input>
+        </form>
         </div>
     );
 }
